7789727b9ae804f20b1eb771d22851907e776bd8
Amanda Xavier <amandavxavier@gmail.com>
2023-08-23 17:46:20 -0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { MediasModule } from './medias/medias.module';
+import { PublicationModule } from './publication/publication.module';
+
+@Module({
+  imports: [PostsModule, MediasModule, PublicationModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
