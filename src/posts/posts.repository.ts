870342f10c0660f78a2d7fe3c9829ac433f7fb27
Amanda Xavier <amandavxavier@gmail.com>
2023-08-26 15:16:00 -0300

    Feat: posts repository

---
+++ src/posts/posts.repository.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PostsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+  create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: createPostDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.post.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.post.findUnique({
+      where: { id },
+      include: {
+        Publication: true,
+      }
+    });
+  }
+
+  update(id: number, updatePostDto: UpdatePostDto) {
+    return this.prisma.post.update({
+      where: { id },
+      data: updatePostDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.post.delete({
+      where: { id },
+    });
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsRepository } from './posts.repository';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly postsRepository: PostsRepository) {}
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
